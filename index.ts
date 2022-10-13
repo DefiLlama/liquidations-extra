36d2cd59f0d3ea8fc3cfff4e3751f8135af6d367
strobie <0xstrobe@protonmail.com>
2022-10-13 21:05:24 +0200

    disable aave

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import { performance } from "perf_hooks";
 import { Liq } from "./DefiLlama-Adapters/liquidations/utils/types";
 import * as _venus from "./DefiLlama-Adapters/liquidations/venus";
-import * as _aaveV2 from "./DefiLlama-Adapters/liquidations/aave-v2";
+// import * as _aaveV2 from "./DefiLlama-Adapters/liquidations/aave-v2";
 
 interface LiquidationAdapter {
   // chain name
@@ -14,7 +14,7 @@ interface LiquidationAdapter {
 }
 
 const venus = (_venus as any).default as LiquidationAdapter;
-const aaveV2 = (_aaveV2 as any).default as LiquidationAdapter;
+// const aaveV2 = (_aaveV2 as any).default as LiquidationAdapter;
 
 dotenv.config();
 
@@ -40,9 +40,9 @@ const handleLiqsReq = (protocol: string, chain: string) => async (_req: Request,
 
 app.get("/venus/bsc", handleLiqsReq("venus", "bsc"));
 
-app.get("/aave-v2/ethereum", handleLiqsReq("aave-v2", "ethereum"));
+// app.get("/aave-v2/ethereum", handleLiqsReq("aave-v2", "ethereum"));
 
-app.get("/aave-v2/polygon", handleLiqsReq("aave-v2", "polygon"));
+// app.get("/aave-v2/polygon", handleLiqsReq("aave-v2", "polygon"));
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
@@ -65,22 +65,23 @@ const fetchVenusLiquidations = async () => {
   );
 };
 
-const fetchAaveV2Liquidations = async () => {
-  await Promise.all(
-    Object.entries(aaveV2).map(async ([chain, liquidationsFunc]) => {
-      try {
-        const _start = performance.now();
-        console.log(`Fetching ${"aave-v2"} data for ${chain}`);
-        const liquidations = await liquidationsFunc.liquidations();
-        await storeCachedLiqs("aave-v2", chain, JSON.stringify(liquidations));
-        const _end = performance.now();
-        console.log(`Fetched ${"aave-v2"} data for ${chain} in ${((_end - _start) / 1000).toLocaleString()}s`);
-      } catch (e) {
-        console.error(e);
-      }
-    }),
-  );
-};
+// const fetchAaveV2Liquidations = async () => {
+//   for (const chain of Object.keys(aaveV2)) {
+//     if (chain === "ethereum") {
+//       continue;
+//     }
+//     try {
+//       const _start = performance.now();
+//       console.log(`Fetching ${"aave-v2"} data for ${chain}`);
+//       const liquidations = await aaveV2[chain].liquidations();
+//       await storeCachedLiqs("aave-v2", chain, JSON.stringify(liquidations));
+//       const _end = performance.now();
+//       console.log(`Fetched ${"aave-v2"} data for ${chain} in ${((_end - _start) / 1000).toLocaleString()}s`);
+//     } catch (e) {
+//       console.error(e);
+//     }
+//   }
+// };
 
 const storeCachedLiqs = async (protocol: string, chain: string, data: string) => {
   delete STORE[protocol];
@@ -96,8 +97,9 @@ const getCachedLiqs = async (protocol: string, chain: string) => {
 setInterval(fetchVenusLiquidations, 1000 * 60 * 30);
 fetchVenusLiquidations();
 
-// run every 30 minutes but delayed by 20 minutes
-setTimeout(() => {
-  setInterval(fetchAaveV2Liquidations, 1000 * 60 * 30);
-  fetchAaveV2Liquidations();
-}, 1000 * 60 * 30);
+// // run every 30 minutes but delayed by 20 minutes
+// setTimeout(() => {
+//   setInterval(fetchAaveV2Liquidations, 1000 * 60 * 30);
+//   fetchAaveV2Liquidations();
+//   // }, 1000 * 60 * 20);
+// }, 0);
